06804f30ba625e96a7967123d40a0bdd08f8271c
Lucas Alencar <lucasalencar.br@gmail.com>
2024-10-22 23:52:03 -0400

    Projeto Streamlit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("Cadastro de clientes")
+
+nome = st.text_input("Nome do cliente", key="nome")
+endereco = st.text_input("Endereço: ", key="endereco")
+data_nasc = st.date_input("Data Nascimento: ", key="dt_nasc")
+tipo = st.selectbox("Tipo de cliente", ["Pessoa física", "Pessoa jurídica"], key="tipo")
+
+cadastro = st.button("Cadastrar")
+
+print(type(data_nasc))
+
+if cadastro:
+    with open("clientes.csv", "a", encoding = "utf8") as arquivo:
+        arquivo.write(f"{nome};{endereco};{data_nasc}\n")
+        st.success("Cliente cadastrado com sucesso!", icon="✅")
